f6f8e8391a35d15a24a199629fcd886e6e38c832
dytakhairani <100221973+dytakhairani@users.noreply.github.com>
2024-06-19 17:38:32 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,15 +4,6 @@ provider "google" {
   region      = var.region
 }
 
-variable "gcp_credentials_file" {}
-variable "project_id" {}
-variable "region" {
-  default = "us-central1"
-}
-variable "zone" {
-  default = "us-central1-a"
-}
-
 resource "google_compute_instance" "default" {
   name         = "instance-name"
   machine_type = "f1-micro"
