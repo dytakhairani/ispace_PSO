35c27a99618af4c557eed9bced6c895942c060c1
dytakhairani <100221973+dytakhairani@users.noreply.github.com>
2024-06-24 20:26:10 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             // 'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST'),
+            'host' => env('DB_HOST', 'ispaceapp.clyksam0s3gg.ap-southeast-2.rds.amazonaws.com'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
