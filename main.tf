74cf03f95d2995f4f6a2eb4198e2a1961ded5169
dytakhairani <100221973+dytakhairani@users.noreply.github.com>
2024-06-25 11:39:47 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-southeast-2" 
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "ispace-app" {
   ami           = "ami-080660c9757080771"
   instance_type = "t2.micro"
   tags = {
